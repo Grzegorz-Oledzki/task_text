6bd559a9410c6bc41d3d1a99729ac07d851974cc
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2024-10-16 21:05:21 +0200

    add views

--- text_task/task/views.py
+++ text_task/task/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from .forms import TextFileForm
+from .utils import mix_word_letters
 
-# Create your views here.
+
+def home(request: WSGIRequest) -> HttpResponse:
+    form = TextFileForm
+    context = {"form": form}
+    if request.method == "POST":
+        form = TextFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES["file"]
+            if file.content_type == "text/plain":
+                for chunk in file.chunks():
+                    text_before = chunk.decode()
+                    text: str = mix_word_letters(chunk.decode().split())
+                context = {"text": text, "text_before": text_before}
+                return render(request, "result.html", context)
+            else:
+                return HttpResponse(f"Wrong format, file should have .txt format")
+    return render(request, "file_upload_form.html", context)
