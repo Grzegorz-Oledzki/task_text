dc43a2d99e18d359590f060121424b6497bb8615
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2024-10-16 21:05:12 +0200

    add utils

---
+++ text_task/task/utils.py
@@ -0,0 +1,18 @@
+import random
+
+
+def mix_word_letters(splitted_words: list[str]) -> str:
+    words = []
+    for word in splitted_words:
+        if len(word) > 3:
+            first_letter, last_letter = word[0], word[-1]
+            rest_of_letters = word[1:-1]
+            mixed_letters = "".join(
+                random.sample(rest_of_letters, k=len(rest_of_letters))
+            )
+            word = f"{first_letter}{mixed_letters}{last_letter}"
+            words.append(word)
+        else:
+            words.append(word)
+    text = " ".join(words)
+    return text
