3e90345a0652004fb473dc533f80d7b18537229c
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2024-10-17 19:40:57 +0200

    changes in utils

--- text_task/task/tests.py
+++ text_task/task/tests.py
@@ -1,4 +1,5 @@
-from .utils import mix_words_letters
+import pytest
+from task.utils import mix_words_letters
 
 test_texts = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -7,12 +8,12 @@ test_texts = [
 ]
 
 
-def test_mix_words_letters():
-    for text in test_texts:
-        split_text = text.split()
-        signs = set(list(text))
-        result = mix_words_letters(split_text)
-        for sign in signs:
-            assert result.count(sign) == text.count(sign)
-        for index, word in enumerate(result.split()):
-            assert len(word) == len(split_text[index])
+@pytest.mark.parametrize("text", test_texts)
+def test_mix_words_letters(text: str):
+    split_text = text.split()
+    signs = set(list(text))
+    result = mix_words_letters(split_text)
+    for sign in signs:
+        assert result.count(sign) == text.count(sign)
+    for index, word in enumerate(result.split()):
+        assert len(word) == len(split_text[index])
